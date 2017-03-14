db2e1b7ff54b4442d1bb54cc58749d44a97af616
Benjamin Lebit <benjamin.lebit@ensimag.grenoble-inp.fr>
2017-03-14 14:05:56 +0100

    ca compile monsieur !

--- OpenCL/binomial.cpp
+++ OpenCL/binomial.cpp
@@ -1,15 +1,13 @@
+#include "binomial.hpp"
 #include "util.hpp"
 #include <err_code.h>
 #include "device_picker.hpp"
-#include <CL/cl.h>
+
+
 
 int main(int argc, char *argv[])
 {
-	//OpenCl variables
-	cl_platform_id platform_id = NULL;		
-	cl_device_id device_id = NULL;		
-	cl_context context = NULL;		
-	cl_command_queue command_queue = NULL;
+
 
 
 	double start_time;      // Starting time
@@ -25,6 +23,49 @@ int main(int argc, char *argv[])
 	
 	Option = (float*)malloc(5*sizeof(float));
 	Model = (float*)malloc(7*sizeof(float));
-	Data = (float*)malloc(depth*sizeof(float));
+	Data = (float*)malloc(Depth*sizeof(float));
 
+	cl::Buffer d_Option ;
+	cl::Buffer d_Model ;
+	cl::Buffer d_Data ;
+	
+	try {
+		cl_uint deviceIndex = 0;
+        	parseArguments(argc, argv, &deviceIndex);
+
+		 // Get list of devices
+        	std::vector<cl::Device> devices;
+        	unsigned numDevices = getDeviceList(devices);
+
+        	// Check device index in range
+        	if (deviceIndex >= numDevices)
+        	{
+          		std::cout << "Invalid device index (try '--list')\n";
+          		return EXIT_FAILURE;
+        	}
+
+		cl::Device device = devices[deviceIndex];
+
+        	std::string name;
+        	getDeviceName(device, name);
+        	std::cout << "\nUsing OpenCL device: " << name << "\n";
+
+        	std::vector<cl::Device> chosen_device;
+        	chosen_device.push_back(device);
+        	cl::Context context(chosen_device);
+        	cl::CommandQueue queue(context, device);
+
+	}  catch ( cl::Error err)
+    {
+        std::cout << "Exception\n";
+        std::cerr << "ERROR: "
+                  << err.what()
+                  << "("
+                  << err_code(err.err())
+                  << ")"
+                  << std::endl;
+    }
+
+    return EXIT_SUCCESS;
+}
 	
---
+++ OpenCL/binomial.hpp
@@ -0,0 +1,32 @@
+//
+// Created by lebitb on 3/14/17.
+//
+
+#ifndef LAB1_SOURCES_BINOMIAL_H
+#define LAB1_SOURCES_BINOMIAL_H
+
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+
+#include <vector>
+
+#define __CL_ENABLE_EXCEPTIONS
+#include "cl.hpp"
+
+extern double wtime();   // returns time since some fixed past point (wtime.c)
+
+// ----------------------------------------------------------------
+//  Constants
+// ----------------------------------------------------------------
+#define ORDER    1024    // Order of the square matrices A, B, and C
+#define AVAL     3.0     // A elements are constant and equal to AVAL
+#define BVAL     5.0     // B elements are constant and equal to BVAL
+#define TOL      (0.001) // tolerance used in floating point comparisons
+#define DIM      2       // Max dim for NDRange
+#define COUNT    1       // number of times to do each multiplication
+#define SUCCESS  1
+#define FAILURE  0
+
+#endif //LAB1_SOURCES_BINOMIAL_H
